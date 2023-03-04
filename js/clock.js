5500a2ec64a37beaf05d95b04c0ecf8d6be26550
Sanghun <po4865@gmail.com>
2023-03-04 17:22:26 +0100

    feat: Add functional clock

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,10 @@
 const clock = document.querySelector('h2#clock');
 
-function sayHello() {
-    console.log("Hello");
+function getClock() {
+    const date = new Date();
+    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
-setInterval(sayHello, 5000);
+getClock();
+setInterval(getClock, 1000);    // repeat, every 1 seconds
+// setTimeout(getClock, 5000);     // once, after 5 seconds

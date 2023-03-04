75fea0d95d082c8cae30c914d3dbc69def5e92dc
Sanghun <po4865@gmail.com>
2023-03-04 14:17:50 +0100

    feat: Practice to get username - Get username with new classname hidden - Show username with hidden

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input:first-child");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
     event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    // console.log(event);
-    console.log(loginInput.value);
-}
-
-function handleLinkClick(event) {
-    event.preventDefault();
-    console.dir(event);
+    greeting.innerText = `"Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);

52b86a636e25bd594c82d62efb7b49099b41e7fa
Sanghun <po4865@gmail.com>
2023-03-04 14:05:14 +0100

    feat: Practice Event

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input:first-child");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
     event.preventDefault();
     const username = loginInput.value;
@@ -8,4 +10,10 @@ function onLoginSubmit(event) {
     console.log(loginInput.value);
 }
 
+function handleLinkClick(event) {
+    event.preventDefault();
+    console.dir(event);
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);

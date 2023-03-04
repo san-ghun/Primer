7b1d4683ba0922a38f297cbda6d99dcd4b2a46dc
Sanghun <po4865@gmail.com>
2023-03-04 14:34:49 +0100

    feat: Practice for localStorage - How to setItem and getItem from localStorage.

--- app.js
+++ app.js
@@ -3,13 +3,32 @@ const loginInput = document.querySelector("#login-form input:first-child");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    greeting.innerText = `"Hello ${username}`;
+    
+    localStorage.setItem(USERNAME_KEY, username);
+
+    paintGreeting(username);
+}
+
+function paintGreeting(username) {
+    greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    // show the greeting
+    paintGreeting(savedUsername);
+}

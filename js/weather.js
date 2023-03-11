bca6ed3a6633fb0f087f0b49fa177bf1c89efafa
Sanghun <po4865@gmail.com>
2023-03-11 20:24:48 +0100

    feat: Add weather div - Update index.html file to add weather div - Update index.html file to add meta data and change title. - Update weather.js file to work with openweather api.

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,23 @@
+const API_KEY = "8fca801914cc80dddf578ceaaed3e728";
+
 function onGeoSuccess(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    console.log(lat, lon);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            if (data.cod === 401) {
+                const msg = data.message;
+                alert(msg);
+                console.log(msg);
+            } else {
+                const weather = document.querySelector("#weather span:first-child");
+                const city = document.querySelector("#weather span:last-child");
+                weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+                city.innerText = data.name;
+            }
+        });
 }
 
 function onGeoFailure(error) {
